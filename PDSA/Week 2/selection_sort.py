653675ae003763f4714a0c786d9ed026e681b690
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-01-27 22:56:57 +0530

    Update selection_sort.py

--- PDSA/Week 2/selection_sort.py
+++ PDSA/Week 2/selection_sort.py
@@ -8,3 +8,17 @@ def selection_sort(lst):
 
 # test the function
 print(selection_sort([5, 2, 8, 1, 9])) 
+
+
+def SelectionSort(L):
+    n = len(L)
+    if n < 1:
+        return L
+    for i in range(n):
+        mpos = i
+        for j in range(i + 1, n):
+            if L[j] < L[mpos]:
+                mpos = j
+        L[i], L[mpos] = L[mpos], L[i]
+    return L
+print(SelectionSort([5, 2, 8, 1, 9])
