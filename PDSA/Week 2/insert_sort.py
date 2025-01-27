7e7aae1dc25416c7156b4710cdffb0aecc5d0584
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-01-27 23:00:50 +0530

    code

--- PDSA/Week 2/insert_sort.py
+++ PDSA/Week 2/insert_sort.py
@@ -11,3 +11,22 @@ def InsertionSort(L):
 
 # test the code
 print(InsertionSort([5, 2, 8, 1, 9]))
+
+
+# recursive insertion sortign
+
+def RecursiveInsertionSort(L, n=None):
+    if n is None:
+        n = len(L)
+    if n <= 1:
+        return L
+    RecursiveInsertionSort(L, n - 1)
+    last = L[n - 1]
+    j = n - 2
+    while j >= 0 and L[j] > last:
+        L[j + 1] = L[j]
+        j -= 1
+    L[j + 1] = last
+    return L
+
+print(RecursiveInsertionSort([5, 2, 8, 1, 9, 11, 19, 18]))
