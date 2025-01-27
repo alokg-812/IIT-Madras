626aafcf202880aeb7e80744a8e75524ab153d54
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-01-27 22:52:53 +0530

    code

--- PDSA/Week 2/selection_sort.py
+++ PDSA/Week 2/selection_sort.py
@@ -1,1 +1,10 @@
+# funtion for sorting without any extra list:
+def selection_sort(lst):
+    for i in range(len(lst)):
+        for j in range(len(lst)):
+            if lst[i] > lst[j]:
+                lst[i], lst[j] = lst[j], lst[i]
+    return lst
 
+# test the function
+print(selection_sort([5, 2, 8, 1, 9])) 
