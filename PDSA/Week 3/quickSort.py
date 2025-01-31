64f7cc44e98529bf6321646353caa560df0494db
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-01-31 10:11:41 +0530

    Create quickSort.py

---
+++ PDSA/Week 3/quickSort.py
@@ -0,0 +1,12 @@
+# code to write basic quick sort function:
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+ls = [9,7,5,3,1,2,4,6,8]
+print(quick_sort(ls))
