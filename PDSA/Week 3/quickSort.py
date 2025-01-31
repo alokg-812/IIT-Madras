517ba9d8df9b2047691131b0ade6c5f4ff7bde90
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-01-31 10:12:08 +0530

    updatees

--- PDSA/Week 3/quickSort.py
+++ PDSA/Week 3/quickSort.py
@@ -1,12 +1,34 @@
-# code to write basic quick sort function:
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quick_sort(left) + middle + quick_sort(right)
+# # code to write basic quick sort function:
+# def quick_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     pivot = arr[len(arr) // 2]
+#     left = [x for x in arr if x < pivot]
+#     middle = [x for x in arr if x == pivot]
+#     right = [x for x in arr if x > pivot]
+#     return quick_sort(left) + middle + quick_sort(right)
 
-ls = [9,7,5,3,1,2,4,6,8]
-print(quick_sort(ls))
+# ls = [9,7,5,3,1,2,4,6,8]
+# print(quick_sort(ls))
+
+# # 
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]  # swap
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]  # swap
+    return i + 1
+
+def quickSort(arr, low, high):
+    if low < high:
+        pivot = partition(arr, low, high)
+        quickSort(arr, low, pivot - 1)
+        quickSort(arr, pivot + 1, high)
+
+arr = [6, 3, 9, 5, 2, 8]
+n = len(arr)
+quickSort(arr, 0, n - 1)
+print(" ".join(map(str, arr)))
