edaa5a672943840a06870c53ec6fa7bcceb4a0ff
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-02-01 18:05:22 +0530

    Create GrPA3.cpp

---
+++ PDSA/GRPA/Week3/GrPA3.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+Node* reverse(Node* head){
+    Node* next;
+    Node* curr = head;
+    Node* prev = NULL;
+    while(curr != NULL){
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+void display(Node* head){
+    while(head != NULL){
+        cout << head->val << " ";
+        head = head->next;
+    }
+    return;
+}
+
+int main(){
+    Node* a = new Node(10);
+    Node* b = new Node(20);
+    Node* c = new Node(30);
+    Node* d = new Node(40);
+    Node* e = new Node(50);
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+    Node* newHead = reverse(a);
+    display(newHead);
+}
