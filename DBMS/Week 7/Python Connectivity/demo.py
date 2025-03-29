b238b3dc0e691fdddf375f434239558055de811d
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-03-29 19:17:47 +0530

    Step 1: Installing psycopg2

--- DBMS/Week 7/Python Connectivity/demo.py
+++ DBMS/Week 7/Python Connectivity/demo.py
@@ -1,1 +1,10 @@
+# install pyscopg2
+# create connection
+# creator cursor
+# execute the query
+# close the cursor
+# close connection
 
+# Step 1: Installing psycopg2(using pip install psycopg2 on terminal)
+import psycopg2
+import os, sys
