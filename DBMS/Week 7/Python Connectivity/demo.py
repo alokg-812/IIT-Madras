2d1a1f15f863e7f94c19b353be9099d5ad4c4cc2
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-03-29 19:27:28 +0530

    step 2: creating connection

--- DBMS/Week 7/Python Connectivity/demo.py
+++ DBMS/Week 7/Python Connectivity/demo.py
@@ -8,3 +8,18 @@
 # Step 1: Installing psycopg2(using pip install psycopg2 on terminal)
 import psycopg2
 import os, sys
+
+
+# Step 2: Creating Connection
+conn = None
+
+conn = psycopg2.connect(
+    database = "test_db",
+    user = "postgres",
+    password = "admin", 
+    host = "localhost", 
+    port = "5432"
+)
+
+print("connected!")
+
