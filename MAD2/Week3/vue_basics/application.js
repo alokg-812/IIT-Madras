65f768a1e3afc384659e336a77e90f00018c305c
alokg-812 <alokji812@gmail.com>
2025-07-11 16:57:33 +0530

    function use

--- MAD2/Week3/vue_basics/application.js
+++ MAD2/Week3/vue_basics/application.js
@@ -4,12 +4,21 @@ var app = new Vue({
     el: "#app",
     data:{
         message:"Hello Vue World",
-        count:0
+        // count:0,
+        visitor_name : "",
+        visitors:[]
     },
     methods:{
         sayHi: function(){
             this.message = "HI";
-            this.count+=1;
+            // this.count+=1;
+            this.visitors.push(this.visitor_name);
+            this.visitor_name="";
+        }
+    },
+    computed:{
+        count: function(){
+            return this.visitors.length;
         }
     }
 })
