6af2522f597d5ad6c78de3dcce0fd0683101cb7a
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-07-20 22:16:27 +0530

    Update GrPA4.bash

--- SystemCommands/Week6/GrPA/GrPA4.bash
+++ SystemCommands/Week6/GrPA/GrPA4.bash
@@ -1,1 +1,30 @@
+script() {
 
+if [ $# -eq 0 ]; then
+    echo "Please provide numbers as arguments."
+    exit 1
+fi
+
+max=$1
+min=$1
+
+for num in "$@"
+do
+    if ! [[ "$num" =~ ^-?([0-9]*[.])?[0-9]+$ ]]; then
+        echo "Invalid input detected: $num"
+        exit 1
+    fi
+
+    if (( $(echo "$num > $max" | bc -l) )); then
+        max=$num
+    fi
+
+    if (( $(echo "$num < $min" | bc -l) )); then
+        min=$num
+    fi
+done
+
+echo "Maximum: $max | Minimum: $min"
+
+
+}
