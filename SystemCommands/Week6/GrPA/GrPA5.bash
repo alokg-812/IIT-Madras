aa1c1d79a647067b050c2ac3c29ad483ab0394d0
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-07-20 22:19:17 +0530

    Update GrPA5.bash

--- SystemCommands/Week6/GrPA/GrPA5.bash
+++ SystemCommands/Week6/GrPA/GrPA5.bash
@@ -1,1 +1,18 @@
+script() {
 
+if [ $# -ne 2 ]; then
+    echo "Please provide exactly two integer arguments."
+    exit 1
+fi
+
+num1=$1
+num2=$2
+for (( i=1; i<=num2; i++ ))
+do
+    product=$(( num1 * i ))
+    echo "${num1}*${i}=${product}"
+done
+
+
+
+}
