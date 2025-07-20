048e99a18163542b26c38c2249b34baf63027028
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-07-20 22:11:05 +0530

    Update GrPA3.bash

--- SystemCommands/Week6/GrPA/GrPA3.bash
+++ SystemCommands/Week6/GrPA/GrPA3.bash
@@ -1,1 +1,12 @@
-
+script() {
+read input
+if [[ "$input" =~ ^-?([0-9]*[.])?[0-9]+$ ]]; then
+    if (( $(echo "$input >= 0" | bc -l) )); then
+        echo "PNUM"
+    else
+        echo "NNUM"
+    fi
+else
+    echo "STRING"
+fi
+}
