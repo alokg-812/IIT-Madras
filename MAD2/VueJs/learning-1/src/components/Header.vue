82b30e4be8b50f4de8ec1df5fc0fb697293ab761
alokg-812 <alokji812@gmail.com>
2025-07-28 11:24:18 +0530

    code implementation

---
+++ MAD2/VueJs/learning-1/src/components/Header.vue
@@ -0,0 +1,21 @@
+<script>
+export default{
+    data(){
+        return{
+            countp:0,
+            countm:0,
+        }
+    },
+}
+</script>
+
+<template>
+    <button @click="countp++">Count is: {{ countp }}</button>
+</template>
+
+<style scoped>
+button{
+    background-color: black;
+    color: white;
+}
+</style>
