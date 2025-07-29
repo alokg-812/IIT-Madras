63b57a26183fed1dd800190873223f7049a0f9a7
alokg-812 <alokji812@gmail.com>
2025-07-29 19:04:58 +0530

    code

---
+++ MAD2/VueJs/learning-2/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Counter />
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ MAD2/VueJs/learning-2/src/components/Counter.vue
@@ -0,0 +1,55 @@
+<template>
+    <h1>Counter</h1>
+
+    <div class="counter-wrapper">
+        <button @click="decreament">DECREASE</button>
+
+        <h2 :class="{'default': isActive, 'bg-danger':hasError}">{{ count }}</h2>
+
+        <button @click="increament">INCREASE</button>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            count:0,
+            isActive:true,
+            hasError:true
+        }
+    },
+    methods:{
+        increament(){
+            this.count++;
+        },
+        decreament(){
+            this.count--;
+        },
+    },
+}
+</script>
+
+
+<style scoped>
+h1{
+    margin: 20px 0;
+    text-align: center;
+}
+
+.counter-wrapper{
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+}
+
+.default{
+    color: red;
+}
+
+.bg-danger{
+    background-color: rgb(8, 255, 243);
+    padding: 10px;
+    border-radius: 8px;
+}
+</style>
