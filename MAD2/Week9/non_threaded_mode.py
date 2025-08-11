e649e024acf59a8eaa65203ebae20643fcbe31f0
alokg-812 <alokji812@gmail.com>
2025-08-11 11:14:37 +0530

    threading blocked

---
+++ MAD2/Week9/non_threaded_mode.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+import time
+
+x = 1
+
+@app.route('/')
+def hello():
+    global x
+    x = x+1
+    print(time.asctime())
+    time.sleep(6)
+    return 'Hellu Buddy, '+ str(x) + time.asctime() + '\n'
+
+if __name__ == '__main__' :
+    app.run(threaded=False, host='0.0.0.0')
