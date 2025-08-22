a7b5b7bd6af01375f24b097f7240be1e344e58fd
alokg-812 <alokji812@gmail.com>
2025-08-22 22:36:10 +0530

    week 6 practice codes

---
+++ SystemCommands/Week6/practice/argumentAndInvokation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This is my second script
+echo I am Invoked as:
+echo $0
+echo Number of Arguments passed to the script:
+echo $#
+echo First Argument passed:
+echo $1
+echo Second Argument passed:
+echo $2
