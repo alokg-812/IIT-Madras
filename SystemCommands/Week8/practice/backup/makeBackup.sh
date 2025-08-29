47d4017449c5195b8de3e8f87994bcd73eb2268e
alokg-812 <alokji812@gmail.com>
2025-08-29 11:25:04 +0530

    old codes of automating scripts

---
+++ SystemCommands/Week8/practice/backup/makeBackup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BFILE=/mnt/c/Users/alokg/Desktop/IIT-Madras/SystemCommands/Week8/practice/backup/mkbackup.log
+echo "Starting backup automatically" >> $BFILE
+date >> $BFILE
+echo "Backup process completed" >> $BFILE
+echo "------------------------" >> $BFILE
