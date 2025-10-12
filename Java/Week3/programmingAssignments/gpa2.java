bd3fe3a07060c9086147121ee0f0a160ac4462bb
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-10-12 22:53:04 +0530

    Create gpa2.java

---
+++ Java/Week3/programmingAssignments/gpa2.java
@@ -0,0 +1,62 @@
+import java.util.*;
+class Shape{
+    public int area() {
+        return 0;
+    }
+    public int volume() {
+        return 0;
+    }
+}
+
+class Rectangle extends Shape{
+    private int w, h;
+//implement Rectangle class
+    public Rectangle(int w, int h) {
+        this.w = w;
+        this.h = h;
+    }
+
+    @Override
+    public int area() {
+        return w * h;
+    }
+}
+
+class Cube extends Shape{
+    private int a;
+    //implement Cube class
+        public Cube(int a) {
+        this.a = a;
+    }
+
+    @Override
+    public int area() {
+        return 6 * a * a;
+    }
+
+    @Override
+    public int volume() {
+        return a * a * a;
+    }
+}
+
+class FClass{
+    private static void caller(Shape s) {
+        if (s instanceof Rectangle)
+            System.out.println(s.area());
+        if (s instanceof Cube)
+            System.out.println(s.volume());
+        //check if s is of type Rectangle
+            // System.out.println(s.area());
+        //check if s is of type Cube
+            // System.out.println(s.volume());
+    }
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+        int a = sc.nextInt();
+        caller(new Rectangle(w, h));
+        caller(new Cube(a));
+    }
+}
