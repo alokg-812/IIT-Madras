19f5a9d315f1a51ff69ecdacee7c461a82ba6d31
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-10-12 23:08:14 +0530

    Create gpa3.java

---
+++ Java/Week3/programmingAssignments/gpa3.java
@@ -0,0 +1,54 @@
+import java.util.*;
+    class BankAccount{
+        int accountNumber;
+        String name;
+        int balance;
+        final int minBalance = 100;
+        private boolean checkMinBalance(int amount){
+            if(balance - amount <= minBalance){
+                return false;
+            }
+            else{
+              return true;
+            }
+        }
+        //Fill the code here
+        //constructor
+        public BankAccount(int accountNumber, String name, int balance) {
+            this.accountNumber = accountNumber;
+            this.name = name;
+            this.balance = balance;
+        }
+        
+        // Public method to print balance
+        public void balance() {
+            System.out.println(balance);
+        }
+    
+        // Deposit method
+        public void deposit(int amount) {
+            balance += amount;
+        }
+    
+        // Withdraw method
+        public void withdraw(int amount) {
+            if (checkMinBalance(amount)) {
+                balance -= amount;
+            } else {
+                System.out.println("Transaction failed");
+            }
+        }
+}
+    class AccountCheck{
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int amnt = sc.nextInt( );
+            int amnt1 = sc.nextInt( );
+            BankAccount b = new BankAccount(1890, "rahul", 1000);
+            b.deposit(amnt);
+            b.balance();
+            b.withdraw(amnt1);
+            b.balance();
+        }
+    
+    }
