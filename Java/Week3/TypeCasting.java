f7669400e98b895f6ac336ed09e2bc3f97999bc8
alokg-812 <alokji812@gmail.com>
2025-10-24 10:06:02 +0530

    Typecasting a child object to its parent object

---
+++ Java/Week3/TypeCasting.java
@@ -0,0 +1,47 @@
+class Employee{
+    String name;
+    int salary;
+
+    public Employee(String name, int salary){
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public double bonus(float percent){
+        return (percent / 100.0) * salary;
+    }
+    public String toString(){
+        return "Name: " + name + ", Salary: " + salary;
+    }
+}
+
+class Manager extends Employee{
+    String secretary;
+    
+    public Manager(String name, int salary, String secretary){
+        super(name, salary);
+        this.secretary = secretary;
+    }
+    public String getSecretary(){
+        return secretary;
+    }
+
+    public double bonus(float percent){
+        return 1.5 * super.bonus(percent);
+    }
+
+    public String toString(){
+        return "Name: " + name + ", Salary: " + salary + ",Secretary: " + secretary;
+    }
+}
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Employee e = new Manager("Ram", 1000, "Ramesh");
+        // System.out.println(e.secretary());  // static type checking don't allow this!
+        /*
+         * The typecasting comes into picture at this stage because although 'e' is a Manager object but still it can't access getSecretary function because of being Employee type reference.
+         */
+        System.out.println(((Manager)e).getSecretary());;
+    }    
+}
