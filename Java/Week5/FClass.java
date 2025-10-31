e16570302090f711f2b4277fa0601e22cf2d634e
alokg-812 <alokji812@gmail.com>
2025-10-31 19:49:24 +0530

    < S extends T, T >

--- Java/Week5/FClass.java
+++ Java/Week5/FClass.java
@@ -1,49 +1,49 @@
-class Player{
-	private String name;
-	private String type;
-	public String getName() {
-		return name;
-	}
-	public String getType() {
-		return type;
-	}
-	public Player(String name, String type) {
-		this.name = name;
-		this.type = type;
-	}
-	public String toString() {
-		return "Player [name=" + name + ", type=" + type + "]";
-	}
-}
-class Captain extends Player{
-	public Captain(String name, String type) {
-		super(name, type);
-	}
-	public String toString() {
-		return "Captain [name=" + getName() + ", type=" + getType() + "]";
-	}
+interface Iterator{
+	public boolean has_next();
+	public Object get_next();
 }
 
-class CopyArrayObjects {
-	public static < S extends T, T > void copy (S[] src, T[] tgt){ //LINE1
-		int i,limit;
-		limit = Math.min(src.length, tgt.length);
-		for (i = 0; i < limit; i++){
-			tgt[i] = src[i];
-		}
+
+class NumList<T extends Number> implements Iterator{
+	private T[] list;
+	private int idx;
+	public NumList(T[] list) {
+		this.list = list;
+		idx = 0;
+	}
+	public boolean has_next() {
+		if(idx < list.length - 1)
+		return true;
+		return false;
+	}
+	public Object get_next() {
+		idx++;
+		return list[idx];
 	}
 }
-   
 public class FClass{
-	public static void main(String[] args) {
-		Captain captain1=new Captain("Virat", "Batting");
-		Captain captain2=new Captain("Hardik", "All Rounder");
-		Captain captain3=new Captain("Jasprit", "Bowling");
-		Captain captain[]= {captain1, captain2, captain3};
-		Player[] player= new Captain[2];
-		CopyArrayObjects.copy(captain,player);
-		for (int i = 0; i < player.length; i++) {
-			System.out.println(player[i]);
-		}
+	// a. 
+	public static double sum(NumList< ? extends Number > lOb);
+	// b.
+	// public static double sum(NumList< Number > lOb);
+	// c. 
+	// public static < T extends Number > double sum(NumList< T > lOb);
+	// d. 
+	// public static double sum(NumList< ? super Number > lOb);
+//  ____________________________________________ //LINE 1: function-header
+{
+	double total = 0;
+	while(lOb.has_next()) {
+		total += ((Number)lOb.get_next()).doubleValue();
 	}
+	return total;
+}
+
+public static void main(String[] args) {
+	Integer[] i_arr = {10, 20, 30, 40, 59};
+	Double[] d_arr = {3.44, 2.65, 6.44, 1.3, 6.78};
+	NumList<Integer> i_list = new NumList<Integer>(i_arr);
+	NumList<Double> d_list = new NumList<Double>(d_arr);
+	System.out.println(sum(i_list) + ", " + sum(d_list));
+   }
 }
