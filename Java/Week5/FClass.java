1442210c3f0f8d12a69da787959ab689d0b130bc
alokg-812 <alokji812@gmail.com>
2025-10-31 18:31:02 +0530

    < S extends T, T >

--- Java/Week5/FClass.java
+++ Java/Week5/FClass.java
@@ -1,37 +1,49 @@
-class NumData {
-    private Object n;
-
-    public NumData(Object n) {
-        this.n = n;
-    }
-
-    public String getMetaInfo() {
-        if (n instanceof Integer) {
-            return "Integer type, value = " + n;
-        } else if (n instanceof Double) {
-            return "Double type, value = " + n;
-        } else if (n instanceof Float) {
-            return "Float type, value = " + n;
-        } else if (n instanceof Character) {
-            return "Character type, value = " + n;
-        } else {
-            return "Other type, value = " + n;
-        }
-    }
+class Player{
+	private String name;
+	private String type;
+	public String getName() {
+		return name;
+	}
+	public String getType() {
+		return type;
+	}
+	public Player(String name, String type) {
+		this.name = name;
+		this.type = type;
+	}
+	public String toString() {
+		return "Player [name=" + name + ", type=" + type + "]";
+	}
+}
+class Captain extends Player{
+	public Captain(String name, String type) {
+		super(name, type);
+	}
+	public String toString() {
+		return "Captain [name=" + getName() + ", type=" + getType() + "]";
+	}
 }
 
-public class FClass {
-    public static void main(String[] args) {
-        Integer iO = 10;
-        Float fO = 3.14f;
-        Character cO = 'A';
-
-        NumData o1 = new NumData(iO);
-        NumData o2 = new NumData(fO);
-        NumData o3 = new NumData(cO);
-
-        System.out.println(o1.getMetaInfo());
-        System.out.println(o2.getMetaInfo());
-        System.out.println(o3.getMetaInfo());
-    }
+class CopyArrayObjects {
+	public static < S extends T, T > void copy (S[] src, T[] tgt){ //LINE1
+		int i,limit;
+		limit = Math.min(src.length, tgt.length);
+		for (i = 0; i < limit; i++){
+			tgt[i] = src[i];
+		}
+	}
+}
+   
+public class FClass{
+	public static void main(String[] args) {
+		Captain captain1=new Captain("Virat", "Batting");
+		Captain captain2=new Captain("Hardik", "All Rounder");
+		Captain captain3=new Captain("Jasprit", "Bowling");
+		Captain captain[]= {captain1, captain2, captain3};
+		Player[] player= new Captain[2];
+		CopyArrayObjects.copy(captain,player);
+		for (int i = 0; i < player.length; i++) {
+			System.out.println(player[i]);
+		}
+	}
 }
