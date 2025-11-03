28f7f4f468371cb9707e647ec81c224386930b8e
alokg-812 <alokji812@gmail.com>
2025-11-03 23:17:47 +0530

    practice codes

--- Java/Week5/Test1.java
+++ Java/Week5/Test1.java
@@ -1,27 +1,28 @@
-public interface Walkable{
+interface Walkable{
 	default void showPaceLength() {
 		System.out.println("Average pace length : 0.4 meters");
 	}
- }
- public class Human implements Walkable{
+}
+class Human implements Walkable{
 	double pace_length = 0.85;
 	public void showPaceLength() {
 		System.out.format("Average pace length : %f meters",pace_length);
  	}
- }
- public class Mammal<T>{
+}
+class Mammal<T>{
 	public String name;
 	public T group;
 	public Mammal(T obj){
 		name = obj.getClass().getSimpleName();
 		group = obj;
-	 }
+	}
 	public void print() {
 		System.out.println(name);
 		group.showPaceLength();
 	}
- }
- public class Test1 {
+}
+
+public class Test1 {
 	public static void main(String[] args) {
 		Mammal<Human> m = new Mammal<Human>(new Human());
 		m.print();
