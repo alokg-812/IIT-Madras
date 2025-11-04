407ea4c1f2fc737ab426a95ea0c76af9c64a8ba6
alokg-812 <alokji812@gmail.com>
2025-11-04 15:44:12 +0530

    java practice codes

--- Java/Week4/FClass.java
+++ Java/Week4/FClass.java
@@ -1,25 +1,27 @@
+package Java.Week4;
+
 interface Printable{
-      public default void print() {
-           System.out.println("not implemented");
-       }
+    public default void print() {
+        System.out.println("not implemented");
     }
+}
 
 abstract class Collection{
-       public void print() {
-           System.out.println("no element");
-       }
-     }
+    public void print() {
+        System.out.println("no element");
+    }
+}
 
 class Queue extends Collection implements Printable{
-       public void print() {
-           super.print();
-           System.out.println("print the queue");
-       }
+    public void print() {
+        super.print();
+        System.out.println("print the queue");
     }
+}
 
-    public class FClass{
-        public static void main(String[] args) {
-           Queue q = new Queue();
-           q.print();
-       }
+public class FClass{
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.print();
     }
+}
