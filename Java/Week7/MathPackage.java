ab28f03b74f30e463c15730f72f6f47f2320ee4e
alokg-812 <alokji812@gmail.com>
2025-11-22 23:55:48 +0530

    Practice codes where package is implemented

---
+++ Java/Week7/MathPackage.java
@@ -0,0 +1,9 @@
+package Java.Week7;
+
+import java.lang.Math;
+public class MathPackage {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(100));
+        System.out.println(Math.pow(2,10));
+    }    
+}
