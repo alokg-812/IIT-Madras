6fc764db803fc9305ff1885f7f00377ff9345698
alokg-812 <alokji812@gmail.com>
2025-11-30 00:28:49 +0530

    practice codes for week 10

---
+++ Java/Week10/FClass.java
@@ -0,0 +1,13 @@
+package Java.Week10;
+
+import java.util.*;
+import java.util.stream.*;
+public class FClass{
+       public static void main(String[] args){
+            var text = "solutions based on test-and-set are low level and prone to programming errors";
+           List<String> words = List.of(text.split(" "));
+           Thread t;
+        //    t = new Thread(__________________); //LINE 1
+           t.start();
+       }
+}
