0a34e434aed981b257749d534084c70160e7a555
alokg-812 <alokji812@gmail.com>
2025-12-07 00:16:18 +0530

    practice codes

--- Java/Week9/Test.java
+++ Java/Week9/Test.java
@@ -1,12 +1,26 @@
 package Java.Week9;
 
 import java.util.*;
+class User{
+      public String name;
+      public String pass;
+      User(String name,String pass){
+            this.name=name;
+            this.pass=pass;
+     }
+     String checkUser(){
+            if(pass.equals("1234")){
+                  return name;
+            }
+            else{
+                  return pass;
+            } 
+      }
+}
 public class Test{
-    public static void main(String[] args){
-              String[] arr=new String[10];
-              arr[0]="Sun";
-              arr[1]="Moon";
-              Optional<String> op=Optional.ofNullable(arr[2]);
-              op.ifPresent(n->System.out.println(n.toUpperCase()));
-    }
+     public static void main(String[] args){
+            Optional<String> op1=Optional.ofNullable(new User(null,"1234").checkUser());
+            Optional<String> op2=Optional.ofNullable(new User("Moon","4321").checkUser());
+            System.out.println(op1.orElse("Sun")+"\n"+op2.orElse("Sun"));
+     }
 }
