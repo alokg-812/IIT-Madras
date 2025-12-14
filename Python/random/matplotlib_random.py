7064798b47c5ff1edc14a7a5b7e9562e7d430428
alokg-812 <alokji812@gmail.com>
2025-12-14 22:46:05 +0530

    random bargraph

---
+++ Python/random/matplotlib_random.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# val = np.random.randint(1,10,10)
+# plt.show()
+
+while True:
+    val = np.random.randint(1,10,10)
+    # plt.bar(range(len(val)), val)
+    plt.show()
